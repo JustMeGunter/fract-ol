f476e15d88f0bd99d9ebb496bd44530702efa6bd
justmegunter <justmegunter@protonmail.com>
2021-08-05 19:56:02 +0200

    no diff

--- src/parse_args.c
+++ src/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: acrucesp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 19:16:59 by acrucesp          #+#    #+#             */
-/*   Updated: 2021/08/05 18:38:00 by acrucesp         ###   ########.fr       */
+/*   Updated: 2021/08/05 19:31:45 by acrucesp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 static int	select_fractal_fnc(char **argv, t_data *data)
 {
-	if (argv[1] && !ft_strncmp(argv[1], "mandelbrot", ft_strlen(argv[1])))
+	if (argv[1] && !ft_strcmp(argv[1], "mandelbrot"))
 		data->fractal = draw_mandelbrot;
-	else if (argv[1] && !ft_strncmp(argv[1], "julia", ft_strlen(argv[1])))
+	else if (argv[1] && !ft_strcmp(argv[1], "julia"))
 		data->fractal = draw_julia;
-	else if (argv[1] && !ft_strncmp(argv[1], "bonus_a", ft_strlen(argv[1])))
+	else if (argv[1] && !ft_strcmp(argv[1], "bonus_a"))
 		data->fractal = draw_bonus_a;
-	else if (argv[1] && !ft_strncmp(argv[1], "bonus_b", ft_strlen(argv[1])))
+	else if (argv[1] && !ft_strcmp(argv[1], "bonus_b"))
 		data->fractal = draw_bonus_b;
 	else
 		return (1);
@@ -32,9 +32,9 @@ void	parse_args(int argc, char **argv, t_data *data)
 	if (argc < 2 || select_fractal_fnc(argv, data))
 	{
 		printf("Enter one of the following sets:\nmandelbrot\njulia ");
-		printf("\nbonua_a\nbonus_b");
+		printf("\nbonus_a\nbonus_b");
 		printf("\n**Julia can be followed by fractal parameters x y\n");
-		printf("**Parameters cant be longer than 8 digits and must be 2.\n");
+		printf("**Parameters cant be longer than 8 chars and must be 2.\n");
 		exit(0);
 	}
 	if (argc > 2)
